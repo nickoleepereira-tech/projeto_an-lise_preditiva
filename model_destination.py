15be553b67f66a2a8135a0ae19d4f6d079bf0c38
nickoleepereira-tech <nickolee.pereira@totvs.com.br>
2025-11-25 17:20:53 -0300

    Create model_destination.py

---
+++ model_destination.py
@@ -0,0 +1,4 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import GradientBoostingRegressor
